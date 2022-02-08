7b4d17f3ff5e4943eeffe25a74f82e0cc2ac86c2
Zac Waite <129032699zw@gmail.com>
2022-02-08 00:46:34 -0500

    Comments, error management

--- src/circuit/element.rs
+++ src/circuit/element.rs
@@ -4,6 +4,7 @@ pub fn create_element(
 	current: (f32, bool),
 	power: (f32, bool)
 ) -> Element{
+	//Constructor
 	let mut new_element = Element{
 		voltage: voltage,
 		resistance: resistance,
@@ -22,26 +23,41 @@ pub struct Element {
 
 impl Element {
 	pub fn print(&self) {
+		//Prints the values of the object
 		let voltage = if self.voltage.1 { self.voltage.0.to_string() } else { String::from("undefined") };
 		let current = if self.current.1 { self.current.0.to_string() } else { String::from("undefined") };
 		let resistance = if self.resistance.1 { self.resistance.0.to_string() } else { String::from("undefined") };
 		let power = if self.power.1 { self.power.0.to_string() } else { String::from("undefined") };
 		println!("<Voltage: {}V> <Resistance: {}Ω> <Current: {}A> <Power: {}J/s>", voltage, resistance, current, power);
 	}
-	pub fn get_voltage(&self) -> (f32, bool) {
-		self.voltage
-	}
-	pub fn get_resistance(&self) -> (f32, bool) {
-		self.resistance
-	}
-	pub fn get_current(&self) -> (f32, bool) {
-		self.current
-	}
+	pub fn get_voltage(&self) -> (f32, bool) {self.voltage}
+	pub fn get_resistance(&self) -> (f32, bool) {self.resistance}
+	pub fn get_current(&self) -> (f32, bool) {self.current}
 	fn try_calc(&mut self) {
-		if self.voltage.1 && self.current.1 && self.resistance.1 {
+		/* 
+		Using the values of the attributes, attempts to find
+		additional values that can be calculated. 
+		*/
+		let mut num_vars = 0; //Number of standard variables calculated
+		if self.voltage.1 {num_vars+=1}
+		if self.current.1 {num_vars+=1}
+		if self.resistance.1 {num_vars+=1}
+
+		//Confirm that the values match if there are additional values
+		if num_vars == 3{
 			let error = self.voltage.0 - self.current.0 * self.resistance.0;
-			if error.abs() < 0.1 {return;}
-			panic!("Invalid value added to element");
+			if error.abs() > 0.1 {panic!("Invalid value added to element");}
+		} else if num_vars>1 && self.power.1 {
+			if self.current.1 && self.voltage.1 {
+				let error = self.power.0 - self.current.0 * self.voltage.0;
+				if error.abs() > 0.1 {panic!("Invalid value added to element");}
+			} else if self.current.1 && self.resistance.1 {
+				let error = self.power.0 - self.current.0.powf(2.0) * self.resistance.0;
+				if error.abs() > 0.1 {panic!("Invalid value added to element");}
+			} else if self.voltage.1 && self.resistance.1 {
+				let error = self.power.0 - self.voltage.0.powf(2.0) / self.resistance.0;
+				if error.abs() > 0.1 {panic!("Invalid value added to element");}
+			}
 		}
 		//Check for ohm's law
 		if self.voltage.1 && self.current.1 && !self.resistance.1 {
@@ -52,11 +68,6 @@ impl Element {
 			self.ohm_law_current();
 		}
 		//Check for power calculations
-		let mut num_vars = 0;
-		if self.voltage.1 {num_vars+=1}
-		if self.current.1 {num_vars+=1}
-		if self.resistance.1 {num_vars+=1}
-
 		if num_vars>1 && !self.power.1 {
 			self.calc_power();
 		} else if num_vars==1 && self.power.1 {
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ fn main() {
     let mut new_circuit = circuit::create_circuit();
     new_circuit.add_element(
         (24.0, true),
-        (5.0, false),
-        (1.0, false),
-        (1.0, true)
+        (24.0, true),
+        (1.0, true),
+        (24.0, true)
     );
     let element = new_circuit.get_elements();
     element[0].print();
