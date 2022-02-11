0ed765a7235b453cdd9f63acaa18d0ccbeb1272a
Zac Waite <129032699zw@gmail.com>
2022-02-11 01:44:37 -0500

    basic terminal program

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,11 @@
 #[path = "circuit/resistor.rs"] mod resistor;
 mod terminal;
+mod circuit;
 
 
 fn main() {
-	let colours = terminal::read_colours();
-    let resistance = resistor::get_resistance_from_colours(colours);
-    println!("Resistance: {}Ω", resistance);
-    // let mut new_circuit = circuit::create_circuit();
-    // new_circuit.add_element(
-    //     (24.0, true),
-    //     (0.0, false),
-    //     (0.0, false),
-    //     (0.0, false)
-    // );
-    // new_circuit.add_element(
-    //     (24.0, true),
-    //     (5.0, true),
-    //     (0.0, false),
-    //     (0.0, false)
-    // );
-    // new_circuit.add_element(
-    //     (24.0, true),
-    //     (0.0, false),
-    //     (0.0, false),
-    //     (5.0, true)
-    // );
-    // let elements = new_circuit.get_elements();
-
-    // for item in elements.iter() {
-    //     item.print()
-    // }
+    terminal::build_circuit();
+	// let colours = terminal::read_colours();
+    // let resistance = resistor::get_resistance_from_colours(colours);
+    // println!("Resistance: {}Ω", resistance);
 }
--- src/terminal.rs
+++ src/terminal.rs
@@ -1,4 +1,7 @@
 use std::io;
+#[path = "./circuit.rs"] mod circuit;
+
+
 pub fn read_colours() -> Vec<String>{
 	println!("Enter colours in one line:");
 	let mut colour_line = String::new();
@@ -13,4 +16,75 @@ pub fn read_colours() -> Vec<String>{
        colours.push(colour.to_string());
 	}
 	return colours;
+}
+pub fn build_circuit() -> circuit::Circuit {
+	let mut new_circuit = circuit::Circuit::new();
+	println!("Enter commands here. \"help\" for more info");
+	loop {
+		let mut command = String::new();
+		io::stdin()
+			.read_line(&mut command)
+			.expect("Failed to read line");
+		match command.trim().to_lowercase().as_str() {
+			"help" => {
+				print_commands();
+			}
+			"add_resistor" => {
+				let new_resistor = get_new_resistor();
+				if new_resistor.2 {
+					new_circuit.add_resistor(new_resistor.0, new_resistor.1);
+				}
+			}
+			"print_elements" => {
+				let elements = new_circuit.get_resistors();
+				for item in elements.iter() {
+					item.print()
+				}
+			}
+			"q"=> {
+				break;
+			}
+			_ => {
+				println!("Invalid Command. q to quit");
+			}
+		}
+	}
+    
+
+
+	return new_circuit;
+}
+
+pub fn print_commands() {
+	println!("add_resistor: begins process to add resistor to curcuit");
+	println!("help: prints different command options");
+	println!("print_elements: prints all elements in circuit");
+	println!("q: quit program");
+}
+
+pub fn get_new_resistor() -> (String, (f32, bool), bool) {
+	let mut name = String::new();
+	let mut value_string = String::new();
+	let mut value: f32;
+
+	println!("Enter resistor name: ");
+	io::stdin()
+		.read_line(&mut name)
+		.expect("Failed to read line");
+	loop {
+		println!("Enter resistor value: ");
+		io::stdin()
+			.read_line(&mut value_string)
+			.expect("Failed to read line");
+		value = match value_string.trim().parse() {
+			Ok(num) => num,
+			Err(_) => {
+				println!("invalid value, try again");
+				continue;
+			}
+		};
+		break;
+	}
+	println!("Added!");
+	return (name.trim().to_string(), (value, true), true);
 }
