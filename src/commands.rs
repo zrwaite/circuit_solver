7d67ca327e7af0ff49e93e76926c367fa8693502
Zac Waite <129032699zw@gmail.com>
2022-02-13 17:58:27 -0500

    Terminal functions and commands

---
+++ src/commands.rs
@@ -0,0 +1,59 @@
+pub mod commands {
+	use crate::terminal_functions;
+	use terminal_functions::terminal_functions as tf;
+	use std::collections::HashMap;
+	
+	pub fn is_single_flag(arg: String) -> bool {
+		if arg.len() < 2 {return false;}
+		else if arg[0..1].to_string().eq("-") {
+			if arg.len()>2 && arg[1..2].to_string().eq("-") {
+				return false;
+			}
+			return true;
+		}
+		false
+	}
+	
+	pub fn is_double_flag(arg: String) -> bool {
+		if arg.len() < 3 {return false;}
+		else if arg[0..2].to_string().eq("--") {return true;}
+		false
+	}
+	
+	pub fn get_flags(command_strings: Vec<String>) -> (HashMap<String, String>, Vec<String>, bool) {
+		let mut success = true;
+		let mut single_flags:HashMap<String, String> = HashMap::new();
+		let mut double_flags:Vec<String> = Vec::new();
+		let mut single_flag = ("", false);
+		for command_string in command_strings.iter() {
+			if is_single_flag(command_string.to_string()) {
+				if single_flag.1 && !single_flags.contains_key(single_flag.0) {
+					// New flag being declared while previous flag has no value
+					tf::println_fail(&format!("Missing params for flag -{}", single_flag.0));
+					success = false;
+				}
+				//Set new flag
+				single_flag = (command_string, true);
+			} else if is_double_flag(command_string.to_string()) {
+				if single_flag.1 && !single_flags.contains_key(single_flag.0) {
+					// New flag being declared while previous flag has no value
+					tf::println_fail(&format!("Missing params for flag -{}", single_flag.0));
+					success = false;
+				}
+				single_flag.1 = false;
+				//Add double flag to list
+				double_flags.push(command_string.to_string());
+			} else if single_flag.1 {
+				if !single_flags.contains_key(single_flag.0) {
+					single_flags.insert(single_flag.0.to_string(), command_string.to_string());
+				} else {
+					single_flags.insert(single_flag.0.to_string(), format!("{} {}", single_flags[single_flag.0], command_string));
+				}
+			} else {
+				tf::println_fail(&format!("Invalid argument '{}'", command_string));
+				success = false;
+			} 
+		}
+		(single_flags, double_flags, success)
+	}
+}
--- src/terminal.rs
+++ src/terminal.rs
@@ -1,9 +1,8 @@
-#[path = "./circuit.rs"] mod circuit;
-mod get_input;
-mod terminal_functions;
-use terminal_functions as tf;
-mod commands;
-
+use crate::commands::commands as commands;
+use crate::circuit::circuit as circuit;
+use crate::get_input::get_input as get_input;
+use crate::terminal_functions::terminal_functions as tf;
+use colored::*;
 
 pub fn build_circuit() -> circuit::Circuit {
 	let mut new_circuit = circuit::Circuit::new();
@@ -24,25 +23,22 @@ pub fn build_circuit() -> circuit::Circuit {
 pub fn run_commands(command_strings: Vec<String>, circuit: &mut circuit::Circuit) -> bool {
 	match command_strings[0].trim().to_lowercase().as_str() {
 		"help" => {
-			print_commands();
+			print_help();
 		}
 		"add_r" => {
-			let new_resistor = commands::get_new_element("resistor");
-			if new_resistor.2 {
-				circuit.add_resistor(new_resistor.0, new_resistor.1);
-			}
+			add_r(command_strings[1..].to_vec(), circuit);
 		}
 		"add_v" => {
-			let new_voltage = commands::get_new_element("voltage");
-			if new_voltage.2 {
-				circuit.add_voltage_source(new_voltage.0, new_voltage.1);
-			}
+			// let new_voltage = commands::get_new_element("voltage");
+			// if new_voltage.2 {
+			// 	circuit.add_voltage_source(new_voltage.0, new_voltage.1);
+			// }
 		}
 		"add_c" => {
-			let new_current = commands::get_new_element("current");
-			if new_current.2 {
-				circuit.add_current_source(new_current.0, new_current.1);
-			}
+			// let new_current = commands::get_new_element("current");
+			// if new_current.2 {
+			// 	circuit.add_current_source(new_current.0, new_current.1);
+			// }
 		}
 		"print" => {
 			circuit.print_elements();
@@ -57,13 +53,66 @@ pub fn run_commands(command_strings: Vec<String>, circuit: &mut circuit::Circuit
 	return true;
 }
 
-pub fn print_commands() {
-	tf::println_info("add_r: begins process to add resistor to curcuit");
-	tf::println_info("add_v: begins process to add voltage to curcuit");
-	tf::println_info("add_c: begins process to add current to curcuit");
+
+
+
+pub fn add_r(command_strings: Vec<String>, circuit: &mut circuit::Circuit) {
+	let flags = commands::get_flags(command_strings);
+	if !flags.2 { //Failed to get flags from command line
+		return;
+	}
+	let mut success = true;
+	let mut r_name = ("".to_string(), false);
+	let mut r_value = (0.0, false);
+	for (key, value) in flags.0 {
+		match key.to_lowercase().as_str() {
+			"n"=> {
+				r_name = (value, true);
+			}
+			"v"=> {
+				let r_value_f: f32;
+				r_value_f = match value.trim().parse() {
+					Ok(num) => num,
+					Err(_) => {
+						tf::println_fail(&format!("Invalid value -v {}", value));
+						success = false;
+						continue;
+					}
+				};
+				r_value = (r_value_f, true);
+			}
+			_ => {
+				tf::println_fail(&format!("Invalid flag -{}", key));
+			}
+		}
+	}
+	for double_flag in flags.1.iter() {
+		match double_flag.to_lowercase().as_str() {
+			_ => {
+				tf::println_fail(&format!("Invalid flag --{}", double_flag));
+			}
+		}
+	}
+	if !success {
+		return;
+	} 
+	if !r_name.1 {
+		r_name.0 = get_input::name("Enter resistor name: ".to_string());
+		r_name.1 = true;
+	}
+	if !r_value.1 {
+		r_value.0 = get_input::value("Enter resistor value: ".to_string());
+		r_value.1 = true;
+	}
+	println!("{}", "Added!".green());
+	circuit.add_resistor(r_name.0, r_value);
+}
+
+pub fn print_help() {
+	tf::println_info("add_r: begins process to add resistor to circuit");
+	tf::println_info("add_v: begins process to add voltage to circuit");
+	tf::println_info("add_c: begins process to add current to circuit");
 	tf::println_info("help: prints different command options");
 	tf::println_info("print: prints all elements in circuit");
 	tf::println_info("q: quit program");
 }
-
-
